4c07577309dd1828d219886fbf7e12dfa612d112
key <elisha.amos316@yahoo.com>
2017-08-04 19:47:33 -0400

    numbers.html

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head><title>Javascript</title>
+<script src= "index.js"  type='text/javascript'></script>
+<script lang="javascript">
+"use strict";
+  var nbrInBootcamp =5;
+  alert(nbrInBootcamp);
+  console.log ("count of bootcamp students:",)
+
+  function displayBootcampStudentAndInstructorCounts() { console.log("Inside the function"); } 
+
+  displayBootcampStudentAndInstructorCounts();
+
+  </script>
+
+  </head>
+  <body>
+
+  </body>
+
+  </html>
